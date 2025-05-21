8d363701d4a19aa68a94051375474ed4007a4c9f
David <jp-david@outlook.com>
2025-05-21 16:22:06 +0200

    Webmag
    V1.0.3

--- script.js
+++ script.js
@@ -9,7 +9,24 @@ function getData() {
      .then((data) => {
        // Traitez les données comme vous le souhaitez
        console.log('Données récupérées du fichier JSON :', data);
-       /// ON ECRIT LE CODE ICI ! 
+       /// ON ECRIT LE CODE ICI !
+       let journal = data.journal.nomJournal;
+      console.log(journal) 
+
+      journal = data.journal.phraseAccroche;
+      console.log(journal)
+
+      journal = data.journal.logo;
+      console.log(journal)
+
+      journal = data.journal.pdf
+      console.log(journal)
+
+      journal =data.journal.texteAppelAction
+      console.log(journal)
+
+      let articlePrincipal = data.journal.articlePrincipal
+      console.log(articlePrincipal)
       
 
       
